8a6dcb69fdfb1069bb69297f8b971e443dca7ad2
Alfredo Bautista <a.bautista@saltosystems.com>
2026-04-03 10:13:05 +0200

    feat: clean up unused sections in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,14 @@ import About from '@/components/sections/about';
 import Info from '@/components/sections/info';
 import Contact from '@/components/sections/contact';
 import PastEvent from '@/components/sections/past-event';
-import CallForPapers from '@/components/sections/call-for-papers';
 import SponsorsSection from '@/components/sections/sponsors-section';
-import Volunteers from '@/components/sections/volunteers';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
-      <CallForPapers />
       <SponsorsSection />
-      <Volunteers />
       <PastEvent />
       <Info />
       <Contact />
